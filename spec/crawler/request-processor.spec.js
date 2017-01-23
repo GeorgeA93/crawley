71c5ca3605109fde226d81edf4406f4d444b5623
GeorgeA93 <george.allen93@outlook.com>
2017-01-23 17:01:02 +0000

    implemented more specs for the request processor

--- spec/crawler/request-processor.spec.js
+++ spec/crawler/request-processor.spec.js
@@ -5,4 +5,92 @@ describe('Request processor', function () {
         const requestProcessor = new RequestProcessor();
         expect(requestProcessor).toBeTruthy();
     });
+    it('can rety a request item', function () {
+        const requestProcessor = new RequestProcessor({
+            maxRetryCount: 1
+        });
+        const requestItem = {
+            retries: 0
+        };
+        const result = requestProcessor._canRetry(requestItem);
+        expect(result).toBe(true);
+    });
+    it('cannot retry a request item whose reties exceed the maxRetryCount', function () {
+        const requestProcessor = new RequestProcessor({
+            maxRetryCount: 1
+        });
+        const requestItem = {
+            retries: 1
+        };
+        const result = requestProcessor._canRetry(requestItem);
+        expect(result).toBe(false);
+    });
+    it('adds a retry to the queue', function () {
+        const requestProcessor = new RequestProcessor({
+            maxRetryCount: 1
+        });
+        const requestItem = {
+            retries: 0
+        };
+        const result = requestProcessor._retry(requestItem);
+        expect(result).toBe(true);
+        expect(requestProcessor.reaminingRequests()).toBe(1);
+    });
+    it('doesnt add a retry to the queue when it exceeds the maxRetryCount', function () {
+        const requestProcessor = new RequestProcessor({
+            maxRetryCount: 1
+        });
+        const requestItem = {
+            retries: 1
+        };
+        const result = requestProcessor._retry(requestItem);
+        expect(result).toBe(false);
+        expect(requestProcessor.reaminingRequests()).toBe(0);
+    });
+    it('can process', function () {
+        const requestProcessor = new RequestProcessor({
+            maxConcurrentRequests: 1
+        });
+        const requestItem = {};
+        const result = requestProcessor._canProcess(requestItem);
+        expect(result).toBe(true);
+    });
+    it('can accept response', function () {
+        const requestProcessor = new RequestProcessor();
+        const response = {
+            statusCode: 200,
+            headers: {
+                "content-type": 'text/html'
+            }
+        };
+        const result = requestProcessor._acceptResponse(response);
+        expect(result).toBe(true);
+    });
+    it('cannot accept invalid status codes response', function () {
+        const requestProcessor = new RequestProcessor();
+        const response = {
+            statusCode: 404,
+            headers: {
+                "content-type": 'text/html'
+            }
+        };
+        const result = requestProcessor._acceptResponse(response);
+        expect(result).toBe(false);
+    });
+    it('cannot accept invalid content types', function () {
+        const requestProcessor = new RequestProcessor();
+        const response = {
+            statusCode: 200,
+            headers: {
+                "content-type": 'text/plain'
+            }
+        };
+        const result = requestProcessor._acceptResponse(response);
+        expect(result).toBe(false);
+    });
+    it('cannot queue invalid request items', function () {
+        const requestProcessor = new RequestProcessor();
+        const result = requestProcessor._canQueue({});
+        expect(result).toBe(false);
+    });
 });
