7cea6ed213cc37b7ebe30a9ad71f4d8032923bc7
GeorgeA93 <george.allen93@outlook.com>
2017-01-24 09:39:45 +0000

    implemented async crawler specs

--- spec/crawler/crawler.spec.js
+++ spec/crawler/crawler.spec.js
@@ -1,6 +1,9 @@
 import Crawler from '../../src/crawler';
 
 describe('Crawler', function () {
+    beforeEach(function (done) {
+        done();
+    }, 2000);
     it('creates an instance', function () {
         const crawler = new Crawler();
         expect(crawler).toBeTruthy();
@@ -10,5 +13,23 @@ describe('Crawler', function () {
             let crawler = new Crawler({ startUrl: 'some silly url' });
         }
         expect(createFn).toThrow(new Error('Invalid start url: some silly url. Could not extract the domain'));
+    });
+    it('can start', function (done) {
+        const crawler = new Crawler();
+        crawler.on('started', () => {
+            done();
+            crawler.stop();
+        });
+        crawler.start();
+    });
+    it('can stop', function (done) {
+        const crawler = new Crawler();
+        crawler.on('started', () => {
+            crawler.stop();
+        });
+        crawler.on('finished', () => {
+            done();
+        });
+        crawler.start();
     });
 });
